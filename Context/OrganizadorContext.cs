fc3ad8380eabaee8d738cde8cc1fd12158529f1d
GabrielKameoka <gabrielkameoka@gmail.com>
2024-09-25 19:33:21 -0300

    Finalizado

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -3,13 +3,23 @@ using TrilhaApiDesafio.Models;
 
 namespace TrilhaApiDesafio.Context
 {
-    public class OrganizadorContext : DbContext
+    public class OrganizadorContext : DbContext   
+
     {
-        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+        private readonly IConfiguration _configuration;
+
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options, IConfiguration configuration) : base(options)
         {
-            
+            _configuration = configuration;
         }
 
         public DbSet<Tarefa> Tarefas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)   
+
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ConexaoPadrao"));
+        }
+
     }
 }
